fdccd3ced14a166821a3e2a28b57d96cbe4a488e
PALR-1216 <palr12162000@gmail.com>
2020-12-09 00:44:24 -0400

    Initial Commit

---
+++ Covid-App/Covid_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Covid_AppApp.swift
+//  Covid-App
+//
+//  Created by Pedro Alejandro on 12/9/20.
+//
+
+import SwiftUI
+
+@main
+struct Covid_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
